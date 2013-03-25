c6367b5d735f6a6fd14d6b99d6c63ddb301a8ba9
pbuttler <pdb7705@g.rit.edu>
2013-03-25 00:17:06 -0300

    Create Validator.java

---
+++ AppSource/editor/Validator.java
@@ -0,0 +1,25 @@
+ package editor;
+
+import java.util.ArrayList;
+
+public class Validator {
+
+  public Validator(){
+		
+	}
+	
+	public ArrayList<Object> parseBuffer(String BString){
+		
+		ArrayList<Object> Contents = new ArrayList<Object>();
+		
+		for (int i = 0; i < BString.length(); i++){
+		    char c = BString.charAt(i);        
+		    if (c == 'c'){
+		    	
+		    }
+		}
+		
+		return Contents;	
+		
+	}
+}
