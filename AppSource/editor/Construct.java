50c01547fcc17d601bdc2a637b30c23e8e09adbb
pbuttler <pdb7705@g.rit.edu>
2013-03-25 00:15:54 -0300

    Create Construct.java

---
+++ AppSource/editor/Construct.java
@@ -0,0 +1,17 @@
+package editor;
+
+public class Construct {
+  
+	private String Contents;
+
+	public void setContents(String contents) {
+		Contents = contents;
+	}
+
+	public String getContents() {
+		return Contents;
+	}
+	
+	
+
+}
