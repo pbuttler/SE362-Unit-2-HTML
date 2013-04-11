64358f08840a9cc02a1f0d6d9a6312def1657f1e
Isioma Nnodum <isioma.nnodum@gmail.com>
2013-04-11 15:48:52 -0400

    Created r1 folder

---
+++ r1/AppSources/buffer/Line.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buffer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Peter
+ */
+public class Line {
+    
+    private String contents;
+    private boolean collapsed;
+    private ArrayList<Line> children;
+    
+    public String getContents(){
+        return contents;
+    }
+    
+    public void setContents( String s ){
+        contents = s;
+    }
+    
+    public boolean isCollapsed(){
+        return collapsed;
+    }
+    
+    public void setCollapsed( boolean b ){
+        collapsed = b;
+        for (Line l : children){
+            l.setCollapsed(b);
+        }
+    }    
+}
