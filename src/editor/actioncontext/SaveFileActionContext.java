ba44c327666e025585ef9ac83c583952de07e919
Danielle Neuberger <drn5447@rit.edu>
2013-04-14 15:55:42 -0400

    started handleSaveAction and OpenFileActionContext

--- src/editor/actioncontext/SaveFileActionContext.java
+++ src/editor/actioncontext/SaveFileActionContext.java
@@ -13,6 +13,15 @@ import java.io.File;
  */
 public class SaveFileActionContext extends GeneralActionContext{
     
+    private File saveFile;
+    private String _title;
+    private String _contents;
+    
     public SaveFileActionContext(){
     }
+    
+    //communicate with the file and rewrite the contents
+    //receive a boolean whether save was successful
+   
+    
 }
