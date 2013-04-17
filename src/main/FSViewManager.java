37529520a8852ee6ea8d5644ef6923606b96bba5
Danielle Neuberger <drn5447@rit.edu>
2013-04-17 18:25:13 -0400

    added image location selection options for insert image also append http:// if it is not already in the url and the image or href is online

--- src/main/FSViewManager.java
+++ src/main/FSViewManager.java
@@ -600,7 +600,9 @@ public class FSViewManager extends JFrame implements ActionListener {
             url = "FILE:///" + url;
         }
         else if (n==1){//then it is on the internet
-            //do nothing to the URL
+            if (!url.contains("http")){
+                url = "http://" + url;
+            }
         }
         InsertHTMLActionContext context = new InsertHTMLActionContext();
 
@@ -688,8 +690,26 @@ public class FSViewManager extends JFrame implements ActionListener {
 
     private void handlePictureAction() {
 
+        Object[] options = {"Local Disk",
+                    "Online"};
+        int n = JOptionPane.showOptionDialog(this,
+            "Where is the image located?",
+            "Insert URL",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options,
+            options[1]);
         String url = JOptionPane.showInputDialog(this,
                 "What is the URL of the picture?", null);
+        if (n==0){//then it is on local disk
+            url = "FILE:///" + url;
+        }
+        else if (n==1){//then it is on the internet
+            if (!url.contains("http")){
+                url = "http://" + url;
+            }
+        }
         String altText = JOptionPane.showInputDialog(this,
                 "What is the alternate text for this picture?", null);
 
